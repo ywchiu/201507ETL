3d33ab3d28c4423738a9b993fb08f33b22e161bc
David <david@largitdata.com>
2015-07-20 15:07:52 +0800

    add course1

---
+++ test.py
@@ -0,0 +1,7 @@
+import test2
+
+def test():
+	print "this is data from test"
+
+if __name__ == '__main__':
+	test()
---
+++ test2.py
@@ -0,0 +1,10 @@
+def test2():
+	print "this is data from test2"
+
+def dummy():
+	print "this is data from dummy"	
+	
+dummy()
+
+if __name__ == '__main__':
+	test2()
